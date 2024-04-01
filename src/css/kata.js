242a6d760414b4335244d6df8c1027a640270939
julio <jcsmerubia@gmail.com>
2024-04-01 23:21:15 +0200

    new kata

---
+++ src/css/kata.js
@@ -0,0 +1,27 @@
+// En este ejercicio de practica "kata" aprenderas a iterar sobre un array usando un bucle o no, no se, ya depende de ti.
+// Esta kata consiste en contar las pendejadas que dice nuestro querido e increiblemente atractivo Huliatzo
+
+// Recibiras como parametro un array de numeros con la cantidad de estup... de chascarrilos que suelta Julio por hora
+
+// ejemplo const chistes = [4,2,0,5,1,8,7,3]
+
+// El ejercicio es simple. debes sumar todos los valores del array y devolver el total de la suma y ya. Eso es todo, mucho animo, te quiero<3!!
+
+let suma = 0;
+
+function totalChistes(array){
+
+}
+// Ahora te has dado cuenta que cada 2 horas Julio susurra un (1) chiste para el mismo.
+// Como tienes un problema con las mates y te fastidia no tener bien las cuentas te toca arreglarlo en la funcion de abajo. 
+// Esta funcion recibe un array como parametro, debe corregir el error de cuenta y devolver el array modificado.
+//pista: solo necesitas tener en cuenta el array una de cada 2 veces
+
+function arregloDeCuenta(array){
+
+}
+
+module.exports = {
+    totalChistes,
+    arregloDeCuenta
+}
---
+++ tests/kata.test.js
@@ -0,0 +1,17 @@
+import { it, expect} from "vitest";
+import { totalChistes, arregloDeCuenta } from "../src/script";
+
+it('should return an existing function "totalChistes"', () =>{
+    expect(totalChistes).toBeDefined();
+});
+
+it("should calculate the correct total number of jokes", ()=>{
+    const jokes = [4,2,0,5,1,8,7,3];
+    expect(totalChistes(jokes)).toBe(30);
+})
+
+it("should return the array with the counting mistake fixed", ()=>{
+    const jokes = [4,2,0,5,1,8,7,3];
+    const fixed = [5,2,1,5,2,8,8,3];
+    expect(arregloDeCuenta(jokes)).toEqual(fixed);
+});
